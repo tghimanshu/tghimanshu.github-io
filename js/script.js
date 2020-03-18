9200812a0c66d2d66e7dfaf01fa1938652f8995c
Himanshu <himanshugohil234@gmail.com>
2020-03-19 00:10:14 +0530

    Added All Project Files

---
+++ js/script.js
@@ -0,0 +1,55 @@
+document.addEventListener('scroll', () => {
+    let x = window.scrollY;
+    if(x>=5){
+        document.querySelector('.nav').style.background = '#333';
+        document.querySelector('.nav').style.padding = '1rem';
+    }else {
+        document.querySelector('.nav').style.background = 'rgba(51,51,51,.1)';
+        document.querySelector('.nav').style.padding = '1.2rem';
+    }
+    if(x>=100){
+        // console.log(x);
+        document.getElementById('scrollTop').style.visibility = 'visible';
+    }else {
+        document.querySelector('#scrollTop').style.visibility = 'hidden';
+    }
+    // console.log(x);
+
+    // Scroll skills
+    // skills bar
+    console.log(document.getElementById('skills').offsetTop-(window.innerHeight*.5));
+    var h = document.getElementById('skills').offsetTop-(window.innerHeight*.8);
+
+    if(window.scrollY >= (h)){
+        // alert("works");
+        $('.sk-bar-inner').each(function(){
+            // console.log($(this).attr('data-width'));
+            $(this).animate({
+                width:$(this).attr('data-width')
+            },1000);
+        });
+    }
+
+
+
+
+
+});
+$('#scrollTop').click(function (e) {
+    let x= this.hash;
+    $('body, html').animate({
+        'scrollTop':0
+    },500);
+});
+
+
+
+
+
+
+
+
+
+
+
+
